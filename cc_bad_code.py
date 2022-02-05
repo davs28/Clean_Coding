e691c5f64093bd5e9d98223f2ddd4d0ac5ef6591
davs28 <77371817+davs28@users.noreply.github.com>
2022-02-05 22:00:23 +0800

    Create cc_bad_code.py

---
+++ cc_bad_code.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+# USE INTENTION-REVEALING NAMES
+
+n = input("How many people?: ")
+
+if (int(n)%2 == 0):
+    print("There are an even number of people.")
+else:
+    print("There are an odd number of people.")
+
+# AVOID DISINFORMATION
+
+sql = 2
+sqa = 2*2
+
+# MAKE MEANINGFUL DISTINCTIONS
+
+def compare(x1, x2):
+    if (x1 > x2):
+        return x1
+    else:
+        return x2gi
+
+# USE PRONOUNCEABLE NAMES
+
+yyyymmdd = date.today()
+
+# USE SEARCHABLE NAMES
+
+days = 0
+for i in range(1, 28):
+    days += 1
+
+# AVOID MENTAL MAPPING
+
+Item.delete()
+Stuff.remove()
